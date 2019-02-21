c581bb76d0059695b0c09ca1b5694848eceba778
applet199 <applet199@gmail.com>
2019-02-21 06:24:39 +0000

    load different application working

--- BusinessLogic/Main.py
+++ BusinessLogic/Main.py
@@ -2,7 +2,11 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 
 from OOPCourseWorkTwo.BusinessLogic.User import User
 
-from OOPCourseWorkTwo.GUI.LoginGUIQtDesigner import Ui_MainWindow
+from OOPCourseWorkTwo.DataAccess.UserDA import UserDA
+
+from OOPCourseWorkTwo.GUI.LoginGUIQtDesigner import Ui_LoginMainWindow
+from OOPCourseWorkTwo.GUI.AdminGUIQtDesigner import Ui_AdminMainWindow
+from OOPCourseWorkTwo.GUI.TeacherGUIQtDesigner import Ui_TeacherMainWindow
 
 import sys
 import sqlite3
@@ -12,26 +16,38 @@ class Main():
 
     def __init__(self):
         app = QtWidgets.QApplication(sys.argv)
-        MainWindow = QtWidgets.QMainWindow()
-        self.ui = Ui_MainWindow()
-        self.ui.setupUi(MainWindow)
+        self.__mainwindow = QtWidgets.QMainWindow()
 
-        connection = self.connect_to_database()
+        self.ui = Ui_LoginMainWindow()
+        self.ui.setupUi(self.__mainwindow)
 
-        User.setup_login_system(self.ui, connection)
-        User.login()
 
-        MainWindow.show()
+        self.ui.pushButton.clicked.connect(self.go_to_admin_application)
+
+        self.__mainwindow.show()
+
         sys.exit(app.exec_())
 
 
-    def connect_to_database(self):
-        try:
-            connection = sqlite3.connect("Database.db")
-            return connection
-        except Error as e:
-            print(e)
-        return None
+    def go_to_admin_application(self):
+        self.__mainwindow.close()
+        self.__mainwindow = QtWidgets.QMainWindow()
+        self.ui = Ui_AdminMainWindow()
+        self.ui.setupUi(self.__mainwindow)
+        self.__mainwindow.show()
+
 
 if __name__ == "__main__":
     Main()
+
+
+
+
+##    @classmethod
+##    def connect_to_database(cls):
+##        try:
+##            connection = sqlite3.connect("Database.db")
+##            return connection
+##        except Error as e:
+##            print(e)
+##        return None
--- GUI/LoginGUIQtDesigner.py
+++ GUI/LoginGUIQtDesigner.py
@@ -8,11 +8,11 @@
 
 from PyQt5 import QtCore, QtGui, QtWidgets
 
-class Ui_MainWindow(object):
-    def setupUi(self, MainWindow):
-        MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(1115, 862)
-        self.centralwidget = QtWidgets.QWidget(MainWindow)
+class Ui_LoginMainWindow(object):
+    def setupUi(self, LoginMainWindow):
+        LoginMainWindow.setObjectName("LoginMainWindow")
+        LoginMainWindow.resize(1115, 862)
+        self.centralwidget = QtWidgets.QWidget(LoginMainWindow)
         self.centralwidget.setObjectName("centralwidget")
         self.layoutWidget = QtWidgets.QWidget(self.centralwidget)
         self.layoutWidget.setGeometry(QtCore.QRect(400, 300, 311, 131))
@@ -51,32 +51,32 @@ class Ui_MainWindow(object):
         self.label_3.setText("")
         self.label_3.setAlignment(QtCore.Qt.AlignCenter)
         self.label_3.setObjectName("label_3")
-        MainWindow.setCentralWidget(self.centralwidget)
-        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        LoginMainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(LoginMainWindow)
         self.menubar.setGeometry(QtCore.QRect(0, 0, 1115, 26))
         self.menubar.setObjectName("menubar")
-        MainWindow.setMenuBar(self.menubar)
-        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        LoginMainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(LoginMainWindow)
         self.statusbar.setObjectName("statusbar")
-        MainWindow.setStatusBar(self.statusbar)
+        LoginMainWindow.setStatusBar(self.statusbar)
 
-        self.retranslateUi(MainWindow)
-        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+        self.retranslateUi(LoginMainWindow)
+        QtCore.QMetaObject.connectSlotsByName(LoginMainWindow)
 
-    def retranslateUi(self, MainWindow):
+    def retranslateUi(self, LoginMainWindow):
         _translate = QtCore.QCoreApplication.translate
-        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
-        self.label.setText(_translate("MainWindow", "UserName"))
-        self.label_2.setText(_translate("MainWindow", "Password"))
-        self.pushButton.setText(_translate("MainWindow", "login"))
+        LoginMainWindow.setWindowTitle(_translate("LoginMainWindow", "MainWindow"))
+        self.label.setText(_translate("LoginMainWindow", "UserName"))
+        self.label_2.setText(_translate("LoginMainWindow", "Password"))
+        self.pushButton.setText(_translate("LoginMainWindow", "login"))
 
 
 if __name__ == "__main__":
     import sys
     app = QtWidgets.QApplication(sys.argv)
-    MainWindow = QtWidgets.QMainWindow()
-    ui = Ui_MainWindow()
-    ui.setupUi(MainWindow)
-    MainWindow.show()
+    LoginMainWindow = QtWidgets.QMainWindow()
+    ui = Ui_LoginMainWindow()
+    ui.setupUi(LoginMainWindow)
+    LoginMainWindow.show()
     sys.exit(app.exec_())
 
